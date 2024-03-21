44bdad19a3e30cdfda11546dffd48ef1c7296f7b
SF4NU <mariusmarius200121@gmail.com>
2024-03-21 18:46:50 +0100

    first commit

---
+++ lezione-1-variabili/main.py
@@ -0,0 +1,21 @@
+name = "Marius"
+print("Hello " + name)
+print(type(name))
+first_name = "Marius"
+last_name = "Ungureanu"
+full_name = first_name + " " + last_name
+print("Hello " + full_name)
+
+age = 22
+age += 1
+print("Your age is: " + str(age))
+print(type(age))
+
+height = 250.5
+print("Your height is: " + str(height) + "cm")
+print(type(height))
+
+human = True
+print(human)
+print("Are you a human: " + str(human))
+print(type(human))
---
+++ lezione-2-assegnazione-multipla/main.py
@@ -0,0 +1,18 @@
+#assegnazione multipla permette di assegnare più variabili nella stessa linea di codice
+
+# name = "Marius"
+# age = 22
+# male = True
+
+name, age, male = "Marius", 22, True
+
+print(name)
+print(age)
+print(male)
+
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
