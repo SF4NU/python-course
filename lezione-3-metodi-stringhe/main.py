62c2360a2b1aedd1d6fa000aff2d082c992b8f59
SF4NU <mariusmarius200121@gmail.com>
2024-03-21 18:55:25 +0100

    finito lezione 3

---
+++ lezione-3-metodi-stringhe/main.py
@@ -0,0 +1,12 @@
+name = "Marius"
+
+print(len(name))
+print(name.find("s"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("M"))
+print(name.replace("M", "D"))
+print(name*3)
