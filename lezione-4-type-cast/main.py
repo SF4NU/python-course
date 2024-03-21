5d985e132062f37b795786acfbc61dea66876a11
SF4NU <mariusmarius200121@gmail.com>
2024-03-21 19:03:00 +0100

    finito lezione 4

---
+++ lezione-4-type-cast/main.py
@@ -0,0 +1,19 @@
+x = 1
+y = 2.0
+z = "3"
+
+print(x)
+print(int(y))
+print(z)
+
+print("X is " + str(x))
+print("Y is " + str(y))
+print("Z is " + str(z))
+
+y = int(y)
+print(y)
+z = int(z) 
+print(z*3)
+x = float(x)
+print(x)
+
